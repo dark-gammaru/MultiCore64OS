f7fafeca34aa8826865e1af6b4860aec231797a0
NaTure77 <xnaudrornfl@gmail.com>
2020-12-05 01:54:18 +0900

    Priniting more strings

--- 00.BootLoader/BootLoader.asm
+++ 00.BootLoader/BootLoader.asm
@@ -24,24 +24,29 @@ START:
 
 	mov si, 0	; SI 레지스터 초기화(문자열 원본)
 	mov di, 0	; DI 레지스터 초기화(문자열 대상)
-
+	mov bx, 0
 .MESSAGELOOP:		; 문자열 출력 루프
 	mov cl, byte[si + MESSAGE1]	; CL레지스터는 CX의 하위 1바이트 의미.
 					; 이 CL에 MESSAGE1의 SI번째 문자를 복사.
 
 	cmp cl, 0 			; 문자의 끝을 의미하는 0과 같은지 비교
 	je .MESSAGEEND			;
-
 	mov byte[es:di],cl	;0이 아니면 0xb800 + di에 문자 출력
-	add si, 1		;
-	add di, 2		;
-	jmp .MESSAGELOOP	;
+	add si, 1		
+	add di, 80*2		
+	cmp di, 80*25*2		
+	jl .MESSAGELOOP	
 
 .MESSAGEEND:
+	mov si, 0
+	add bx, 4
+	mov di, bx
+	cmp bx, 160
+	jl  .MESSAGELOOP
 	jmp $		; 현재 위치에서 무한루프 수행
 
 MESSAGE1:	DB 'HELLO MY CUSTOM OS', 0	; 출력 문자열 정의
-
+INDEX:		DB 0					;
 ;mov byte [es: 0x00], 'M'	; ES 세그먼트:오프셋 0xB800: 0x0000에 M을 복사, 여기에 es: 없으면 디폴트로 DS 세그먼트로 지정됨.
 ;mov byte [es: 0x01], 0x42	; ES 세그먼트:오프셋 0xB800: 0x0001에 0x4A(빨간배경에 밝은녹색)복사
 ;mov byte [es: 0x02], 'M'	; ES 세그먼트:오프셋 0xB800: 0x0002에 M을 복사
